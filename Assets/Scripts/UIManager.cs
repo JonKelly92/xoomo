689a321e09371d72d5c59abd1b314b1107208d11
JonKelly92 <jonkelly@proton.me>
2022-09-15 15:38:38 -0400

    UI score text
    Added more text meshes to display score in the 3 different game play states so I can test that the code is working correctly before swapping in more complex UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,10 +4,19 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI tapScoreLeft;
-    [SerializeField] private TextMeshProUGUI tapScoreRight;
+    [SerializeField] private TextMeshProUGUI score_Center_LeftPlayer;
+    [SerializeField] private TextMeshProUGUI score_Center_RightPlayer;
+
+    [SerializeField] private TextMeshProUGUI score_Left_LeftPlayer;
+    [SerializeField] private TextMeshProUGUI score_Left_RightPlayer;
+
+    [SerializeField] private TextMeshProUGUI score_Right_LeftPlayer;
+    [SerializeField] private TextMeshProUGUI score_Right_RightPlayer;
+
     [SerializeField] private Button leftButton;
     [SerializeField] private Button rightButton;
+
+    private GamePlayState gamePlayState;
 
     private void Awake()
     {
@@ -31,11 +40,41 @@ public class UIManager : MonoBehaviour
 
     private void EventManager_OnGamePlayStateChanged(GamePlayState state)
     {
+        gamePlayState = state;
+
         // TODO: move players to Left or Right or Center based on the game state
         // possibly have an animation
         // send GamePlayStateChangeCompleted when the ui is ready for the game to continue
 
         // DEBUG --------------
+
+        score_Center_LeftPlayer.gameObject.SetActive(false);
+        score_Center_RightPlayer.gameObject.SetActive(false);
+        score_Left_LeftPlayer.gameObject.SetActive(false);
+        score_Left_RightPlayer.gameObject.SetActive(false);
+        score_Right_LeftPlayer.gameObject.SetActive(false);
+        score_Right_RightPlayer.gameObject.SetActive(false);
+
+        Debug.Log("No Null");
+
+        switch (state)
+        {
+            case GamePlayState.Center:
+                score_Center_LeftPlayer.gameObject.SetActive(true);
+                score_Center_RightPlayer.gameObject.SetActive(true);
+                break;
+
+            case GamePlayState.Left:
+                score_Left_LeftPlayer.gameObject.SetActive(true);
+                score_Left_RightPlayer.gameObject.SetActive(true);
+                break;
+
+            case GamePlayState.Right:
+                score_Right_LeftPlayer.gameObject.SetActive(true);
+                score_Right_RightPlayer.gameObject.SetActive(true);
+                break;
+        }
+
         EventManager.GamePlayStateChangeCompleted();
         // ---------------------
     }
@@ -45,10 +84,41 @@ public class UIManager : MonoBehaviour
     }
     private void EventManager_OnTapScoreUpdate(int tapScore, Location location)
     {
+        // DEBUG: using text meshes to test the code before adding in more complex UI elements
+
         if (location == Location.Left)
-            tapScoreLeft.text = tapScore.ToString();
-        else
-            tapScoreRight.text = tapScore.ToString();
+        {
+            switch(gamePlayState)
+            {
+                case GamePlayState.Center:
+                    score_Center_LeftPlayer.text = tapScore.ToString();
+                    break;
+                case GamePlayState.Left:
+                    score_Left_LeftPlayer.text = tapScore.ToString();
+                    break;
+                case GamePlayState.Right:
+                    score_Right_LeftPlayer.text = tapScore.ToString();
+                    break;
+            }
+        }
+        else if (location == Location.Right)
+        {
+            switch (gamePlayState)
+            {
+                case GamePlayState.Center:
+                    score_Center_RightPlayer.text = tapScore.ToString();
+                    break;
+                case GamePlayState.Left:
+                    score_Left_RightPlayer.text = tapScore.ToString();
+                    break;
+                case GamePlayState.Right:
+                    score_Right_RightPlayer.text = tapScore.ToString();
+                    break;
+            }
+        }
+
+        // ---------------------------------------------
+
     }
 
     private void LeftBtnPress() => EventManager.LeftButtonPressed();
